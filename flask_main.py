052af3676e92afa1e60cb9fa4837fc5e7c89be09
benedektoth_rollbar <benedek.toth@rollbar.com>
2020-12-03 16:08:47 +0100

    Added errorhandlers to the flask_main.py

--- flask_main.py
+++ flask_main.py
@@ -89,6 +89,23 @@ def delete_user():
 
     return None
 
+@app.errorhandler(errors.UserAlreadyExistError)
+def handle_user_already_exists(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+@app.errorhandler(errors.UserDoesNotExistError)
+def handle_user_doesnt_exists(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+@app.errorhandler(errors.PermissionDenied)
+def handle_permission_denied(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 def Main():
     global user_df
--- resources/errorhandlers.py
+++ resources/errorhandlers.py
@@ -1,23 +1,20 @@
-import sys
-sys.path.append("../")
-from flask_main import app
+from flask_main import app, errors
 from flask import jsonify
-from .errors import UserAlreadyExistError, UserDoesNotExistError, PermissionDenied
 
-@app.errorhandler(UserAlreadyExistError)
+@app.errorhandler(errors.UserAlreadyExistError)
 def handle_user_already_exists(error):
     response = jsonify(error.to_dict())
     response.status_code = error.status_code
     return response
 
-@app.errorhandler(UserDoesNotExistError)
-def handle_user_already_exists(error):
+@app.errorhandler(errors.UserDoesNotExistError)
+def handle_user_doesnt_exists(error):
     response = jsonify(error.to_dict())
     response.status_code = error.status_code
     return response
 
-@app.errorhandler(PermissionDenied)
-def handle_user_already_exists(error):
+@app.errorhandler(errors.PermissionDenied)
+def handle_permission_denied(error):
     response = jsonify(error.to_dict())
     response.status_code = error.status_code
     return response
