c9f4abbeae5177d2591100f10fa189957c882c03
benedektoth_rollbar <benedek.toth@rollbar.com>
2020-12-17 15:45:52 +0100

    Added useralready_exist error generation
    Added method parameter to api_call
    Headers are now set automatically
    
    Test file's location is set automatically

--- restful_project/auto_test/auto_test.py
+++ restful_project/auto_test/auto_test.py
@@ -1,23 +1,81 @@
 import requests
 import json
 import pandas as pd
+import random
+from time import sleep, time
+import threading
+import os
+
+if os.path.exists('test_data.csv'):
+    TEST_FILE_PATH = 'test_data.csv'
+elif os.path.exists('auto_test/test_data.csv'):
+    TEST_FILE_PATH = 'auto_test/test_data.csv'
+else:
+    raise FileExistsError("TEST_FILE_PATH IS INVALID")
 
 USERS = pd.DataFrame(columns=['user', 'email', 'password', 'new_password'])
-USERS = pd.read_csv('test_data.csv')
+USERS = pd.read_csv(TEST_FILE_PATH)
 URL = 'http://127.0.0.1:5000/'
 
 ENDPOINT_LIST = ['signup', 'login', 'users', 'user']
 
 
-def Main():
-    signup = api_test(ENDPOINT_LIST[0], USERS)
+def auto_test_run():
+    sleep(2)
+    api_call(ENDPOINT_LIST[0], 'POST', USERS)
+    logins = api_call(ENDPOINT_LIST[1], 'POST', USERS)
+    token = {}
+    for index, login in logins.items():
+        resp = json.loads(login.text)
+        token[resp['user_id']] = resp['token']
+
+
+
+
+    # generate_useralreadyexist_errors(continous=True)
     return
 
 
-def api_test(endpoint, users=USERS):
+def get_new_token_thread(logins):
+    while 1:
+        get_new_token(logins)
+        sleep(10)
+
+
+def get_new_token(logins):
+    for login in logins:
+        resp = json.loads(login.text)
+        if time() > resp['valid_to']-600:
+            api_call(f'user_id/{resp["user_id"]}', 'GET')
+            logins = api_call(ENDPOINT_LIST[1], 'POST', USERS)
+            break
+    return logins
+
+
+def generate_useralreadyexist_errors(runs=10, continous=False, delay='auto'):
+
+    signup_response = {}
+    first_run = True
+    while continous or first_run:
+        if delay == 'auto':
+            delay_s = random.randrange(10, 100, 1)
+        elif isinstance(delay, int):
+            delay_s = delay
+        else:
+            delay_s = 10
+        for i in range(runs):
+            signup_response[i] = api_call('signup', 'POST', USERS)
+        first_run = False
+        print(f'error_generation is sleeping for {delay_s}')
+        sleep(delay_s)
+
+    return signup_response
+
+
+def api_call(endpoint, method, users=USERS, token=None):
 
     url = URL+endpoint
-    headers = {'Content-Type': 'application/json'}
+    headers = get_headers(token)
     response = {}
     for index, user in users.iterrows():
         usr = user['user']
@@ -31,11 +89,23 @@ def api_test(endpoint, users=USERS):
                     "password": pw,
                     "new_password": new_pw
                     }
-        response[index] = requests.request("POST", url, headers=headers, data=json.dumps(payload))
+        response[index] = requests.request(method, url, headers=headers, data=json.dumps(payload))
 
         print(response[index].text)
 
     return response
 
+
+def get_headers(token):
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    if token is not None:
+        headers['Authorization'] = f'Bearer {token}'
+
+    return headers
+
+
 if __name__ == '__main__':
-    Main()
+    auto_test_run()
