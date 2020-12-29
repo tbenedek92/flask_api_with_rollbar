b9800e3acf42499c04311a86657c4ec964b093b4
benedektoth_rollbar <benedek.toth@rollbar.com>
2020-12-29 10:02:30 +0100

    Exception propagation is set Username & email address are now being captured and sent to rollbar
    added setter for rollbar_person in CustomRequest class

--- restful_project/flask_restful_app.py
+++ restful_project/flask_restful_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, got_request_exception, Request
+from flask import Flask, got_request_exception, Request, request
 import os
 import rollbar.contrib.flask as rb_flask
 import rollbar
@@ -13,6 +13,7 @@ from restful_project.restful_resources.routes import initialize_routes
 
 
 app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = True
 api = Api(app, errors=errors)
 app.config.setdefault('JWT_SECRET_KEY', 'goX3LAcMwR8AuAyGUVGvfPrEwJgxWtvA9hf2NA4P7J7kV27fx')
 bcrypt = Bcrypt(app)
@@ -34,22 +35,32 @@ def init_rollbar():
         environment='flask_test',
         root=os.path.dirname(os.path.realpath(__file__)),
         allow_logging_basics_config=False,
-        code_version=get_git_sha()
+        code_version=get_git_sha(),
+        capture_username=True,
+        capture_email=True,
+        capture_ip=True,
+
     )
     rollbar.events.add_payload_handler(custom_exception_handler)
     rollbar.report_message('Rollbar initialized succesfully', level='debug')
     got_request_exception.connect(rb_flask.report_exception, app)
 
 
-class CustomRequest(Request):
 
-    DEFAULT_RB_ID = 0
-    DEFAULT_RB_USERNAME= 'default_test_rollbar_user'
-    DEFAULT_RB_EMAIL = 'default_test@rollbar.com'
+class CustomRequest(Request):
+    _rb_id = '1'
+    _username = 'default_test_rollbar_user'
+    _email = 'default_test@rollbar.com'
 
     @property
-    def rollbar_person(self, rb_id=DEFAULT_RB_ID, username=DEFAULT_RB_USERNAME, email=DEFAULT_RB_USERNAME):
-        return {'id': rb_id, 'username': username, 'email': email}
+    def rollbar_person(self):
+        return {'id': self._rb_id, 'username': self._username, 'email': self._email}
+
+    @rollbar_person.setter
+    def rollbar_person(self, rb_person_dict):
+        self._rb_id = rb_person_dict['rb_id']
+        self._username = rb_person_dict['username']
+        self._email = rb_person_dict['email']
 
 
 @app.route('/')
@@ -64,7 +75,7 @@ import auto_test
 if __name__ == '__main__':
     app.request_class = CustomRequest
 
-    threading.Thread(target=auto_test.auto_test_run).start()
+   # threading.Thread(target=auto_test.auto_test_run).start()
     app.run()
     # from .auto_test.auto_test import auto_test_run
     # auto_test_run()
